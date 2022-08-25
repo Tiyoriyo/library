9859375d335c5f3fe01be762dea9ee272b29e874
Vadim Kostrov <vmkvo15@outlook.com>
2022-08-25 12:28:42 +0400

    Create Book constructor

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+const bookContainer = document.querySelector('.main');
+
 const aboutFace = {
     name: 'About Face',
     author: 'David Hackworth',
     status: 'Reading',
 }
 
-let myLibrary = [aboutFace, ];
+let myLibrary = [aboutFace];
 
-function Book() {
-    // the constructor
+const book1 = document.createElement('div');
+const book1Title = document.createElement('div');
+const book1Author = document.createElement('div');
+const book1Status = document.createElement('status');
+
+function Book(name, author, status) {
+    this.name = name;
+    this.author = author;
+    this.status = Boolean(status);
+};
+
+function addBookToLibrary(book) {
+
 }
 
-function addBookToLibrary() {
-    // do stuff here
-}
+
+
 
