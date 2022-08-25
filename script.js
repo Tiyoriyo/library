3ae33aeed62a818b63778021abc7b5ccb7686d0f
Vadim Kostrov <vmkvo15@outlook.com>
2022-08-25 14:04:10 +0400

    Add multiple functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const bookContainer = document.querySelector('.main');
+const createBook = document.createElement('div');
+
+
 
 const aboutFace = {
     name: 'About Face',
@@ -6,7 +8,13 @@ const aboutFace = {
     status: 'Reading',
 }
 
-let myLibrary = [aboutFace];
+const aboutPace = {
+    name: 'About Pace',
+    author: 'David Hackworth',
+    status: 'Reading',
+}
+
+let myLibrary = [aboutFace, aboutPace];
 
 
 function Book(name, author, status) {
@@ -18,6 +26,38 @@ function Book(name, author, status) {
 function addBookToLibrary(book) {
     myLibrary.push(book)
 };
+
+function updateLiveLibrary(obj) {
+    const bookContainer = document.querySelector('.main');
+    obj.forEach(element => {
+        const createBook = document.createElement('div');
+        createBook.style.display = 'grid';
+        createBook.style.gridTemplateColumns = 'inherit';
+        createBook.style.gridColumn = '1 / 4';
+        
+        propertyToDiv(obj, element);
+       
+        bookContainer.appendChild(createBook);
+    });
+
+};
+
+updateLiveLibrary(myLibrary);
+
+function propertyToDiv(obj, element) {
+    const bookName = document.createElement('div');
+    const bookAuthor = document.createElement('div');
+    const bookRead = document.createElement('div');
+
+    bookName.textContent = element['name'];
+    createBook.appendChild(bookName);
+    bookAuthor.textContent = element['author'];
+    createBook.appendChild(bookAuthor);
+    bookRead.textContent = element['status'];
+    createBook.appendChild(bookRead);
+    
+}
+
 
 
 
